ece2806bcaa70b8c9e130ed2f4d6082954607395
andzelika86992 <86992@student.ue.poznan.plgit config --global user.email 86992@student.ue.poznan.pl>
2022-03-29 20:56:56 +0200

    Dodano kalkulator z dodawaniem

--- test.py
+++ test.py
@@ -3,4 +3,12 @@ To będzie plik na którym będziemy pracowali z repo
 def hello(name):
 	return "Hello" + str(name)
 
-print ("Hello world")
+def dodaj(a,b)
+	wynik  float(a) + float(b)
+	return wynik
+
+pierwsza = input()
+druga = input()
+
+print (dodaj(pierwsza, druga))
+
